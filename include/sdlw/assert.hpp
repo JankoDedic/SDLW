fabb385e291837eeb4f707672772d60b4b156316
Janko Dedic <jankodedic7@gmail.com>
2019-07-18 18:12:01 +0200

    Align #defines

--- include/sdlw/assert.hpp
+++ include/sdlw/assert.hpp
@@ -6,6 +6,6 @@
 #   define SDL_ASSERT_LEVEL SDLW_ASSERT_LEVEL
 #endif
 
-#define SDLW_ASSERT SDL_assert
-#define SDLW_ASSERT_RELEASE SDL_assert_release
+#define SDLW_ASSERT          SDL_assert
+#define SDLW_ASSERT_RELEASE  SDL_assert_release
 #define SDLW_ASSERT_PARANOID SDL_assert_paranoid
