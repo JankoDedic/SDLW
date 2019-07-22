5145b1c2d3c58df655ec49b50c6bc4fa8113214b
Janko Dedic <jankodedic7@gmail.com>
2019-07-22 11:18:43 +0200

    Refactor main header

--- include/sdlw/sdlw.hpp
+++ include/sdlw/sdlw.hpp
@@ -1,39 +1,29 @@
 #pragma once
 
 #include <sdlw/assert.hpp>
+#include <sdlw/audio.hpp>
+#include <sdlw/blend_mode.hpp>
 #include <sdlw/clipboard.hpp>
 #include <sdlw/cpu_info.hpp>
+#include <sdlw/error.hpp>
+#include <sdlw/events.hpp>
 #include <sdlw/filesystem.hpp>
+#include <sdlw/game_controller.hpp>
+#include <sdlw/gesture.hpp>
+#include <sdlw/joystick.hpp>
+#include <sdlw/keyboard.hpp>
+#include <sdlw/keycode.hpp>
+#include <sdlw/message_box.hpp>
+#include <sdlw/mouse.hpp>
+#include <sdlw/pixels.hpp>
 #include <sdlw/platform.hpp>
+#include <sdlw/power.hpp>
+#include <sdlw/rectangle.hpp>
+#include <sdlw/render.hpp>
+#include <sdlw/scancode.hpp>
 #include <sdlw/subsystem.hpp>
-
-    #include <sdlw/audio.hpp>
-
-    #include <sdlw/events.hpp>
-
-        #include <sdlw/game_controller.hpp>
-
-        #include <sdlw/gesture.hpp>
-
-        #include <sdlw/joystick.hpp>
-
-        #include <sdlw/keyboard.hpp>
-        #include <sdlw/keycode.hpp>
-        #include <sdlw/scancode.hpp>
-
-        #include <sdlw/mouse.hpp>
-
-        #include <sdlw/touch.hpp>
-
-    #include <sdlw/power.hpp>
-
-    #include <sdlw/timer.hpp>
-
-    #include <sdlw/video.hpp>
-    #include <sdlw/blend_mode.hpp>
-    #include <sdlw/message_box.hpp>
-    #include <sdlw/rectangle.hpp>
-    #include <sdlw/render.hpp>
-    #include <sdlw/surface.hpp>
-
-        #include <sdlw/pixels.hpp>
+#include <sdlw/surface.hpp>
+#include <sdlw/timer.hpp>
+#include <sdlw/touch.hpp>
+#include <sdlw/types.hpp>
+#include <sdlw/video.hpp>
