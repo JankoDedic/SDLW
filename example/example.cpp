70260eb7bca47507367d6d6b543a10a687a8fd57
Janko Dedic <jankodedic7@gmail.com>
2019-07-22 12:12:06 +0200

    Update example files

--- example/example.cpp
+++ example/example.cpp
@@ -1,3 +1,5 @@
+#include <vector>
+
 #include <sdlw/sdlw.hpp>
 #include <sdlw/image.hpp>
 #include <sdlw/ttf.hpp>
@@ -5,17 +7,13 @@
 using namespace std;
 using namespace std::chrono;
 using namespace sdlw;
-using namespace sdlw::events;
-using namespace sdlw::time;
-using namespace sdlw::video;
 
 static auto points = std::vector<point>();
 
 void handle_event(const event &e) {
-    using namespace sdlw::events::mouse;
     if (e.type() == event_type::mouse_motion) {
         const auto state = e.mouse_motion().state();
-        const auto pressed = state & button_mask(button::left);
+        const auto pressed = state & button_mask(mouse_button::left);
         if (static_cast<bool>(pressed)) {
             const auto x = e.mouse_motion().x();
             const auto y = e.mouse_motion().y();
@@ -43,10 +41,11 @@ void run() {
     // Initialize the subsystems
     const auto sdlw_guard = sdlw::subsystem(sdlw::subsystem_flags::video);
     // Create the window
-    const auto win_size = size_of(display::usable_bounds(0)) / 2;
+    const auto win_size = size_of(display_usable_bounds(0)) / 2;
     const auto win_bounds = rectangle{50, 50, win_size.w, win_size.h};
     auto win = window("title", win_bounds, window_flags::shown);
     get_window(win.id()).set_title("new title");
+    win.set_hit_test([] (auto...) { return hit_test_result::draggable; });
     // Create the renderer
     auto rend = renderer(win, renderer_flags::accelerated);
     auto e = event();
--- example/second.cpp
+++ example/second.cpp
@@ -1,4 +1,11 @@
-/* #include <sdlw/sdlw.hpp> */
-/* #include <sdlw/image.hpp> */
-/* #include <sdlw/ttf.hpp> */
-#include <sdlw/mouse.hpp>
+#include <sdlw/rectangle.hpp>
+
+void foo() {
+    auto r1 = sdlw::rectangle{};
+    auto r2 = sdlw::rectangle{};
+    r1 == r2;
+
+    auto p1 = sdlw::point{};
+    auto p2 = sdlw::point{};
+    p1 == p2;
+}
