893f4616a7ea9e61f62be2e84ef9e89773feff8b
Janko Dedic <jankodedic7@gmail.com>
2019-08-28 10:19:01 +0200

    Use array type for unique_ptr to indicate a string is returned

--- include/sdlw/filesystem.hpp
+++ include/sdlw/filesystem.hpp
@@ -10,19 +10,19 @@
 
 namespace sdl {
 
-inline auto base_path() -> std::unique_ptr<const char, detail::sdl_string_deleter>
+inline auto base_path() -> std::unique_ptr<const char[], detail::sdl_string_deleter>
 {
     if (const auto path = SDL_GetBasePath()) {
-        return std::unique_ptr<const char, detail::sdl_string_deleter>(path);
+        return std::unique_ptr<const char[], detail::sdl_string_deleter>(path);
     } else {
         throw error{};
     }
 }
 
-inline auto get_pref_path(const char* org, const char* app) -> std::unique_ptr<const char, detail::sdl_string_deleter>
+inline auto get_pref_path(const char* org, const char* app) -> std::unique_ptr<const char[], detail::sdl_string_deleter>
 {
     if (const auto path = SDL_GetPrefPath(org, app)) {
-        return std::unique_ptr<const char, detail::sdl_string_deleter>(path);
+        return std::unique_ptr<const char[], detail::sdl_string_deleter>(path);
     } else {
         throw error{};
     }
